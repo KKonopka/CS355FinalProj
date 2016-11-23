2df6dba77a9d1a40e5755715a0063468f3cb9704
algburton <algburton@gmail.com>
2016-11-22 21:56:44 -0500

    Main updated with program framework

--- CS355FinalProj/Main.c
+++ CS355FinalProj/Main.c
@@ -3,8 +3,27 @@
 main(int av, char* args[])
 {
 	FILE* file;
-	Py_Initialize();
-	file = fopen("camera.py", "r");
-	PyRun_SimpleFile(file, "camera.py");
-	Py_Finalize();
+	
+	int Done = 0;
+	while (Done)
+	{
+		//Take Picture
+		Py_Initialize();
+		file = fopen("camera.py", "r");
+		PyRun_SimpleFile(file, "camera.py");
+		Py_Finalize();
+
+		//Prompt for Continue
+		printf("Picture Taken, Continue(y/n)? ");
+		int iTest = getchar();
+
+		//Process Result
+		if (iTest == 'n')
+		{
+			Done = 1;
+		}
+		putchar(iTest);
+		printf("\n");
+	}
+	
 }
