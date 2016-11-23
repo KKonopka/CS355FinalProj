8415de784e733ff9e86f514c46796cbae94abacb
kkonopka <kkonopka@my.ccsu.edu>
2016-11-22 22:17:53 -0500

    Add python file for the sensor

---
+++ CS355FinalProj/Sensorss.py
@@ -0,0 +1,26 @@
+import time
+import grovepi
+import sys
+import os
+import fcntl
+from picamera import PiCamera
+
+pir_sensor = 8
+fl = fcntl.fcntl(sys.stdin.fileno(), fcntl.F_GETFL)
+fcntl.fcntl(sys.stdin.fileno(), fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+grovepi.pinMode(pir_sensor, "INPUT")
+flag = 0
+while flag == 0:
+	try:
+		if grovepi.digitalRead(pir_sensor):
+			camera = PiCamera()
+			camera.start_preview()
+			time.sleep(2)
+			camera.capture('image.jpg')
+		flag = sys.stdin.read()
+		time.sleep(2)
+	except IOError:
+		print "Error"
+				
+
