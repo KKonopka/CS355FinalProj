7b4658a3fa6b9a3228898aca0df21ada4d95ee2d
algburton <algburton@gmail.com>
2016-11-28 21:57:03 -0500

    testing

--- CS355FinalProj/Main.c
+++ CS355FinalProj/Main.c
@@ -20,95 +20,95 @@ int main(int av, char* args[])
 
 
 		//Process Picture
-		//FILE *fp = fopen("image.jpg", "rb");
-		//if (fp == NULL)
-		//{
-		//	fprintf(stderr, "cannot open image file\n");
-		//	exit(1);
-		//}
-		//fseek(fp, 0, SEEK_END);
-		//if (ferror(fp)) {
-		//	fprintf(stderr, "fseek() failed\n");
-		//	int r = fclose(fp);
-		//	if (r == EOF) {
-		//		fprintf(stderr, "cannot close file handler\n");
-		//	}
-		//	exit(1);
-		//}
-		//int flen = ftell(fp);
-		//if (flen == -1) {
-		//	perror("error occurred");
-		//	int r = fclose(fp);
-		//	if (r == EOF) {
-		//		fprintf(stderr, "cannot close file handler\n");
-		//	}
-		//	exit(1);
-		//}
-		//fseek(fp, 0, SEEK_SET);
-		//if (ferror(fp)) {
+		FILE *fp = fopen("image.jpg", "rb");
+		if (fp == NULL)
+		{
+			fprintf(stderr, "cannot open image file\n");
+			exit(1);
+		}
+		fseek(fp, 0, SEEK_END);
+		if (ferror(fp)) {
+			fprintf(stderr, "fseek() failed\n");
+			int r = fclose(fp);
+			if (r == EOF) {
+				fprintf(stderr, "cannot close file handler\n");
+			}
+			exit(1);
+		}
+		int flen = ftell(fp);
+		if (flen == -1) {
+			perror("error occurred");
+			int r = fclose(fp);
+			if (r == EOF) {
+				fprintf(stderr, "cannot close file handler\n");
+			}
+			exit(1);
+		}
+		fseek(fp, 0, SEEK_SET);
+		if (ferror(fp)) {
 
-		//	fprintf(stderr, "fseek() failed\n");
-		//	int r = fclose(fp);
+			fprintf(stderr, "fseek() failed\n");
+			int r = fclose(fp);
 
-		//	if (r == EOF) {
-		//		fprintf(stderr, "cannot close file handler\n");
-		//	}
+			if (r == EOF) {
+				fprintf(stderr, "cannot close file handler\n");
+			}
 
-		//	exit(1);
-		//}
+			exit(1);
+		}
 
-		//char data[flen + 1];
-		//int size = fread(data, 1, flen, fp);
+		char data[flen + 1];
+		int size = fread(data, 1, flen, fp);
 
-		//if (ferror(fp)) {
+		if (ferror(fp)) {
 
-		//	fprintf(stderr, "fread() failed\n");
-		//	int r = fclose(fp);
+			fprintf(stderr, "fread() failed\n");
+			int r = fclose(fp);
 
-		//	if (r == EOF) {
-		//		fprintf(stderr, "cannot close file handler\n");
-		//	}
+			if (r == EOF) {
+				fprintf(stderr, "cannot close file handler\n");
+			}
 
-		//	exit(1);
-		//}
+			exit(1);
+		}
 
-		//int r = fclose(fp);
+		int r = fclose(fp);
 
-		//if (r == EOF) {
-		//	fprintf(stderr, "cannot close file handler\n");
-		//}
+		if (r == EOF) {
+			fprintf(stderr, "cannot close file handler\n");
+		}
 
-		//MYSQL *con = mysql_init(NULL);
+		MYSQL *con = mysql_init(NULL);
 
-		//if (con == NULL)
-		//{
-		//	fprintf(stderr, "mysql_init() failed\n");
-		//	exit(1);
-		//}
-		//if (mysql_real_connect(con, "localhost", "root", "pass*",
-		//	"imagedb", 0, NULL, 0) == NULL)
-		//{
-		//	finish_with_error(con);
-		//}
+		if (con == NULL)
+		{
+			fprintf(stderr, "mysql_init() failed\n");
+			exit(1);
+		}
+		if (mysql_real_connect(con, "localhost", "root", "pass*",
+			"imagedb", 0, NULL, 0) == NULL)
+		{
+			finish_with_error(con);
+		}
 
-		//char chunk[(2 * size) + 1];
-		//mysql_real_escape_string(con, chunk, data, size);
+		char chunk[(2 * size) + 1];
+		mysql_real_escape_string(con, chunk, data, size);
 
-		//char *st = "INSERT INTO images(img) VALUES('%s')";
-		//size_t st_len = strlen(st);
+		char *st = "INSERT INTO images(img) VALUES('%s')";
+		size_t st_len = strlen(st);
 
-		//char query[st_len + 2 * size + 1];
-		//int len = snprintf(query, st_len + 2 * size + 1, st, chunk);
+		char query[st_len + 2 * size + 1];
+		int len = snprintf(query, st_len + 2 * size + 1, st, chunk);
 
-		//if (mysql_real_query(con, query, len))
-		//{
-		//	fprintf(stderr, "%s\n", mysql_error(con));
-		//	mysql_close(con);
-		//	exit(1);
-		//}
+		if (mysql_real_query(con, query, len))
+		{
+			fprintf(stderr, "%s\n", mysql_error(con));
+			mysql_close(con);
+			exit(1);
+		}
 
-		//mysql_close(con);
-		//exit(0);
+		mysql_close(con);
+		exit(0);
 
 		//Prompt for Continue
 		printf("Picture Taken, Continue(y/n)? ");
